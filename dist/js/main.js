6ed2849e2e26de06160966ca140dc2438af82331
Kanwar gill <gill.nihal@live.com>
2021-01-02 17:17:32 -0500

    menu button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,14 @@
 // DOM items selection
 
-const menuBtn = document.querySelector(".btn-menu");
+const menuBtn = document.querySelector(".menu-btn");
 
 const menu = document.querySelector(".menu");
 
-const menuNav = document.querySelector(".btn-nav");
+const menuNav = document.querySelector(".menu-nav");
 
-const menuBranding = document.querySelector(".btn-branding");
+const menuBranding = document.querySelector(".menu-branding");
 
-const navItems = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".btn-line");
 
 // Set initial state of menu
 
@@ -18,6 +18,22 @@ menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    //Set Menu this.state.
+    showMenu = true;
   } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    //Set Menu this.state.
+    showMenu = false;
   }
 }
