f632152ab298f3094db02d4099ef7b67740fe102
Kanwar gill <gill.nihal@live.com>
2021-01-02 13:08:44 -0500

    Menu

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+// DOM items selection
+
+const menuBtn = document.querySelector(".btn-menu");
+
+const menu = document.querySelector(".menu");
+
+const menuNav = document.querySelector(".btn-nav");
+
+const menuBranding = document.querySelector(".btn-branding");
+
+const navItems = document.querySelectorAll(".nav-items");
+
+// Set initial state of menu
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+  } else {
+  }
+}
