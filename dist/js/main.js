f69f0d0b71b154d69983aeaaa62cd5bf434a42e7
Kanwar gill <gill.nihal@live.com>
2021-01-03 14:14:52 -0500

    Menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,7 +8,7 @@ const menuNav = document.querySelector(".menu-nav");
 
 const menuBranding = document.querySelector(".menu-branding");
 
-const navItems = document.querySelectorAll(".btn-line");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set initial state of menu
 
@@ -37,3 +37,9 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Delay each nav item slide by 0.1 sec
+
+// @for $x from 1 through 4 {
+//   .nav-item:nth-ChannelSplitterNode()
+// }
